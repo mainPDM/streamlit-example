c6ac53b97732f449689264db638c8fa63690a806
mainPDM <92794260+mainPDM@users.noreply.github.com>
2023-03-17 15:24:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,14 @@ import pandas as pd
 import numpy as np
 from subprocess import check_output
 import os
+import requests
 
 st.title("Appel réseau")
 link=st.text_area('Lien',value='MyLink')
-st.button('Appel')#, on_click = lancement_conversion)
+rt=st.button('Appel', on_click = appel_reseau)
+response_text=st.text_area('Réponse',value=rt)
+
+def appel_reseau():
+  res = requests.get('paste your link here')
+  response = json.loads(res.text)
+  return response
